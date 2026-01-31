76607630deeb8cbb70b4a448d3d7ebcf809f603f
ian <ian@wowitsian.com>
2026-01-31 01:31:35 -0600

    feat: add system tray platform module

---
+++ src/platform/win32/tray.cpp
@@ -0,0 +1,103 @@
+// Lumos - System tray module
+// Copyright (C) 2026 Ian Dirk Armstrong
+// License: GPL v2
+
+#include "tray.h"
+
+namespace lumos::platform {
+
+Tray::~Tray()
+{
+    destroy();
+}
+
+bool Tray::create(HWND parent, UINT callback_msg)
+{
+    if (created_) return true;
+
+    hwnd_ = parent;
+    callback_msg_ = callback_msg;
+
+    nid_.cbSize = sizeof(NOTIFYICONDATAW);
+    nid_.hWnd = hwnd_;
+    nid_.uID = 1;
+    nid_.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid_.uCallbackMessage = callback_msg_;
+    nid_.hIcon = LoadIconW(nullptr, IDI_APPLICATION); // Default icon for now
+    wcscpy_s(nid_.szTip, L"Lumos - Gamma Control");
+
+    if (!Shell_NotifyIconW(NIM_ADD, &nid_)) {
+        return false;
+    }
+
+    created_ = true;
+    return true;
+}
+
+void Tray::destroy()
+{
+    if (!created_) return;
+
+    Shell_NotifyIconW(NIM_DELETE, &nid_);
+    created_ = false;
+}
+
+void Tray::showMenu()
+{
+    POINT pt;
+    GetCursorPos(&pt);
+
+    HMENU menu = CreatePopupMenu();
+    if (!menu) return;
+
+    AppendMenuW(menu, MF_STRING, ID_OPEN, L"Open Lumos");
+    AppendMenuW(menu, MF_SEPARATOR, 0, nullptr);
+    AppendMenuW(menu, MF_STRING, ID_RESET, L"Reset Gamma");
+    AppendMenuW(menu, MF_SEPARATOR, 0, nullptr);
+    AppendMenuW(menu, MF_STRING, ID_EXIT, L"Exit");
+
+    // Required for menu to work properly from tray
+    SetForegroundWindow(hwnd_);
+
+    UINT cmd = TrackPopupMenu(
+        menu,
+        TPM_RETURNCMD | TPM_NONOTIFY | TPM_RIGHTBUTTON,
+        pt.x, pt.y,
+        0, hwnd_, nullptr);
+
+    DestroyMenu(menu);
+
+    // Handle selection
+    switch (cmd) {
+    case ID_OPEN:
+        if (on_open) on_open();
+        break;
+    case ID_RESET:
+        if (on_reset) on_reset();
+        break;
+    case ID_EXIT:
+        if (on_exit) on_exit();
+        break;
+    }
+
+    // Required for menu to dismiss properly
+    PostMessageW(hwnd_, WM_NULL, 0, 0);
+}
+
+bool Tray::handleMessage(WPARAM wParam, LPARAM lParam)
+{
+    (void)wParam;
+
+    switch (LOWORD(lParam)) {
+    case WM_RBUTTONUP:
+        showMenu();
+        return true;
+    case WM_LBUTTONDBLCLK:
+        if (on_open) on_open();
+        return true;
+    }
+
+    return false;
+}
+
+} // namespace lumos::platform
---
+++ src/platform/win32/tray.h
@@ -0,0 +1,51 @@
+// Lumos - System tray module
+// Copyright (C) 2026 Ian Dirk Armstrong
+// License: GPL v2
+
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <shellapi.h>
+#include <functional>
+
+namespace lumos::platform {
+
+class Tray {
+public:
+    Tray() = default;
+    ~Tray();
+
+    // Initialize tray icon
+    bool create(HWND parent, UINT callback_msg);
+
+    // Remove tray icon
+    void destroy();
+
+    // Show context menu at cursor position
+    void showMenu();
+
+    // Handle tray callback message, returns true if handled
+    bool handleMessage(WPARAM wParam, LPARAM lParam);
+
+    // Menu item callbacks
+    std::function<void()> on_open;
+    std::function<void()> on_reset;
+    std::function<void()> on_exit;
+
+    // Menu item IDs
+    static constexpr UINT ID_OPEN = 1001;
+    static constexpr UINT ID_RESET = 1002;
+    static constexpr UINT ID_EXIT = 1003;
+
+private:
+    HWND hwnd_ = nullptr;
+    UINT callback_msg_ = 0;
+    bool created_ = false;
+    NOTIFYICONDATAW nid_{};
+};
+
+} // namespace lumos::platform
